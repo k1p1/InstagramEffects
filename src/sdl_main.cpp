52aebb0209eeb31bbe4828ae366af0368566dbd0
Viktor Ketipov <viktor.ketipov@gmail.com>
2017-02-07 13:25:47 +0200

    Added SDL2 Image and PNG loading.

--- src/sdl_main.cpp
+++ src/sdl_main.cpp
@@ -1,9 +1,13 @@
-#ifdef __APPLE__
-#include <SDL2/SDL.h>
-#else
-#include <SDL.h>
-#endif
 #include <stdio.h>
+#include <string>
+
+#ifdef __APPLE__
+    #include <SDL2/SDL.h>
+    #include <SDL2_image/SDL_image.h>
+#else
+    #include <SDL.h>
+    #include <SDL_image.h>
+#endif
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -16,72 +20,91 @@ bool init()
 {
 	bool success = true;
 
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 		success = false;
 	}
 	else
 	{
-		gWindow = SDL_CreateWindow( "Instagram Effects", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
+		gWindow = SDL_CreateWindow("Instagram Effects", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if(gWindow == NULL)
 		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 			success = false;
 		}
-		else
-		{
-			gScreenSurface = SDL_GetWindowSurface( gWindow );
-		}
+        else
+        {
+            int imgFlags = IMG_INIT_PNG;
+            if(!(IMG_Init(imgFlags) & imgFlags))
+            {
+                printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                success = false;
+            }
+            else
+            {
+                gScreenSurface = SDL_GetWindowSurface(gWindow);
+            }
+        }
 	}
 
 	return success;
 }
 
-bool loadImage(const char* imageFilePath)
+SDL_Surface* loadImageToSurface(const char* imageFilePath)
 {
-	bool success = true;
+    SDL_Surface* optimizedSurface = NULL;
     
-	gHelloWorld = SDL_LoadBMP( imageFilePath );
-	if( gHelloWorld == NULL )
-	{
-		printf( "Unable to load image %s! SDL Error: %s\n", imageFilePath, SDL_GetError() );
-		success = false;
-	}
-
-	return success;
+    SDL_Surface* loadedSurface = IMG_Load(imageFilePath);
+    if(loadedSurface == NULL)
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", imageFilePath, IMG_GetError() );
+    }
+    else
+    {
+        optimizedSurface = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, NULL );
+        if( optimizedSurface == NULL )
+        {
+            printf( "Unable to optimize image %s! SDL Error: %s\n", imageFilePath, SDL_GetError() );
+        }
+        
+        SDL_FreeSurface(loadedSurface);
+    }
+    
+    return optimizedSurface;
 }
 
 void close()
 {
-	SDL_FreeSurface( gHelloWorld );
+	SDL_FreeSurface(gHelloWorld);
 	gHelloWorld = NULL;
 
-	SDL_DestroyWindow( gWindow );
+	SDL_DestroyWindow(gWindow);
 	gWindow = NULL;
 
 	SDL_Quit();
 }
 
-int main( int argc, char* args[] )
+int main(int argc, char* args[])
 {
-	if( !init() )
+	if(!init())
 	{
-		printf( "Failed to initialize!\n" );
+		printf("Failed to initialize!\n");
 	}
 	else
 	{
-		if( !loadImage("data/hello_world.bmp") )
+        gHelloWorld = loadImageToSurface("data/hello_world.png");
+		if(gHelloWorld == NULL)
 		{
-			printf( "Failed to load image!\n" );
+			printf("Failed to load image!\n");
 		}
 		else
 		{
-			SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
 			
-			SDL_UpdateWindowSurface( gWindow );
+			SDL_UpdateWindowSurface(gWindow);
 
-			SDL_Delay( 2000 );
+			SDL_Delay(2000);
 		}
 	}
 
